7f0e2b5cef35ba63b4ec7c8b9f051fd9d1efa4cf
Chris Nance <christopher.nance@icloud.com>
2022-04-19 19:56:39 -0500

    Changed everything

---
+++ ARM_Plan_Modifier.py
@@ -0,0 +1,104 @@
+info = '''
+#==============================================================================#
+title               :ARM_Manager.py
+description         :Allows lite-weight management of ARM accounts.
+author              :Chris Nance
+date                :2022-09-04
+version             :1.0
+usage               :python ARM_Manager.py
+notes               :See requirments.txt for list of dependencies.
+python_version      :3.10
+#==============================================================================#
+'''
+
+## MODULES
+from time import sleep
+import TerminalControllerAPI as tc
+import PySimpleGUI as sg
+from colorama import Fore, init
+init(convert=True)
+for num in range(1,100): print('\n')
+print(Fore.MAGENTA + "Output Window Loaded. Hello! :)" + Fore.RESET)
+
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+notProcessed = []
+numberOfCustomersInFile = 0
+processing = False
+
+def createWindow():
+
+    elements = [
+        [sg.Text('Please select the Customer CSV File you wish to use', key='info')],
+
+        [sg.Input(), sg.FileBrowse(file_types=(("CSV Files", "*.csv"),), key="CSV_IN"), sg.Button("Submit")],
+    ]
+
+    return sg.Window('Test App', elements)
+
+def createSecondWindow():
+    listElements = ['Discontinue', 'Terminate', 'Enable 1-Time Message', 'Disable 1-Time Message']
+    elements = [
+        [sg.Text('What would you like to do?', key='info'), sg.DropDown(listElements, size=(20,50), key='menuofstuff')],
+
+        [sg.ProgressBar(numberOfCustomersInFile, key='progress')],
+        [sg.Text('FastPass Accounts Queued for Modification: '+str(numberOfCustomersInFile), key='status', justification='c')],
+
+        [sg.Submit()],
+    ]
+
+    return sg.Window('Test App', elements)
+
+window = createWindow()
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event=="Exit":
+        exit()
+    elif event == "Submit":
+        print(values["CSV_IN"])
+        dataframe = tc.validate_input(values['CSV_IN'])
+        if dataframe.empty == False:
+            print()
+            window['info'].update('FILE HAS BEEN VALIDATED')
+            numberOfCustomersInFile = len(dataframe.index)
+            break
+        else:
+            print('nope')
+
+window.close()
+window = createSecondWindow()
+
+while True:
+    event, values = window.read()
+    if event == sg.WIN_CLOSED or event=="Exit":
+        print("The following customer codes were not processed...\n" + notProcessed)
+        exit()
+    elif event == "Submit" and processing != True and values['menuofstuff'] == 'Enable 1-Time Message':
+        processing = True
+        print(Fore.LIGHTYELLOW_EX + 'Starting script... Please click on the Python tab and keep the Terminal Controller in-focus.')
+        sleep(5)
+        for index, row in dataframe.iterrows():
+            tc.openNewSale()
+
+            window['status'].update('Modifying fastpass accounts...' + str(index) + '/' + str(numberOfCustomersInFile))
+            window['progress'].update(index)
+
+            customerCode = tc.convertFPN(row['CustomerCode'])
+
+            if customerCode[0] in numbers:
+                tc.lookupAccount(customerCode)
+                tc.enableOneTimeMessage(customerCode)
+            else:
+                print(Fore.RED + "[SKIPPING]\t account with code:", str(customerCode) + Fore.RESET)
+                notProcessed.append(customerCode)
+
+            sleep(1)
+        print(Fore.LIGHTYELLOW_EX + 'Script has completed. Press any key to close this window.' + Fore.RESET)
+        input()
+        exit()
+    elif event == "Submit" and processing != True and values['menuofstuff'] == 'Discontinue':
+        print(Fore.RED + "Not Supported." + Fore.RESET)
+    else:
+        print(Fore.RED + "Not Supported." + Fore.RESET)
+
---
+++ TerminalControllerAPI.py
@@ -0,0 +1,125 @@
+info = '''
+#========================================================================================#
+title               :TerminalControllerAPI.py
+description         :sends keystrokes/mouseclicks to the Terminal Controller
+author              :Chris Nance
+date                :2022-04-17
+version             :1.0
+usage               :python TerminalControllerAPI.py
+notes               :do not pass arguments; script will open a GUI. Do not distribute.
+                    :relies on modules: mouse, keyboard, colorama
+python_version      :3.10
+#========================================================================================#
+'''
+
+
+import mouse, keyboard
+from colorama import Fore, init
+init(convert=True)
+import pandas as pd
+from time import sleep
+import clipboard as cb
+
+
+keystrokeDictionary = {
+
+    '''
+    This dictionary houses all the 'Hot Keys' or 'Key Shortcuts' (in SiteManager) and links
+    them to proper descriptions of the button. Please ensure that the screen called
+    '2022 Python Screen' is properly setup and if any changes need to be made they shopuld
+    be done in this script, NOT in SiteManager. 
+
+    To read this dictionary simply do the following: 
+    hotkey_for_lite_wash_fastpass = keystrokeDictionary['FastPass_Lite']
+    '''
+
+
+    'FastPass_Lite':        ['Alt', 'a'], # Lite FastPass
+    'FastPass_Starter':     ['Alt', 'b'], # Starter FastPass
+    'FastPass_Pro':         ['Alt', 'c'], # Pro FastPass
+    'FastPass_Legend':      ['Alt', 'd'], # Legend FastPass
+
+    'FindAccount_Name':     ['Alt', 'e'], # Find Account with Search Name (Last,First)
+    'FindAccount_FPcode':   ['Alt', 'f'], # Find Account with FastPass Number
+    'FindAccount_PCcode':   ['Alt', 'g'], # Find Account with PC Code
+    'FindAccount_CC':       ['Alt', 'h'], # Find Account with Credit Card Number
+    'FastPass_Phone':       ['Alt', 'i'], # Find Account with Phone Number
+
+    'Tender_ARM_CC':        ['Alt', 'j'], # Tender Current Sale w/ ARM CC on File
+    'Tender_Cash':          ['Alt', 'k'], # Tender Current Sale w/ CASH
+
+    'Discontinue':          ['Alt', 'l'], # Add 'Discontinue' item to the current sale
+    'Terminate':            ['Alt', 'm'], # Add 'Terminate' item to the current sale
+    'Switch':               ['Alt', 'n'], # Add 'Switch' item to the current sale
+
+    'OneTimeMsg_Enable':    ['Alt', 'o'], # Enable the one-time message to show up for the customer. 
+    'OneTimeMsg_Disable':   ['Alt', 'p'], # Disable the one-time message to show up for the customer. (This should not be used, the customer will disable it via transitions in SiteManager)
+
+    'OpenNewSale':          ['Alt', 'q'], # Opens a new sale
+    'ReleaseSale':          ['Alt', 'r'], # Releases the current sale
+
+}
+
+def convertFPN(FPN):
+    splitStr = FPN.split('+')
+    if len(splitStr) == 2:
+        returnableStr = splitStr[0]+splitStr[1]
+        return returnableStr
+    else:
+        splitStr = FPN.split('-')
+        returnableStr = splitStr[0]+splitStr[1]
+        return returnableStr
+
+def validate_input(file):
+    try:
+        return pd.read_csv(file)
+    except UnicodeDecodeError:
+        return False
+    except FileNotFoundError:
+        return False
+    except Exception:
+        return False
+
+def openNewSale():
+    keyboard.press(keystrokeDictionary['OpenNewSale'][0])
+    keyboard.press(keystrokeDictionary['OpenNewSale'][1])
+    sleep(0.2)
+    keyboard.press(keystrokeDictionary['OpenNewSale'][0])
+    keyboard.press(keystrokeDictionary['OpenNewSale'][1])
+    sleep(0.2)
+
+def lookupAccount(customerCode):
+    if customerCode == False:
+        print(Fore.RED + "[SKIPPING]\t account with code:", str(customerCode) + Fore.RESET)  
+    else:
+        print(Fore.LIGHTBLUE_EX + "[LOADING]\t Account with code:", customerCode + Fore.RESET)
+        openNewSale()
+        keyboard.press(keystrokeDictionary['FindAccount_FPcode'][0])
+        keyboard.press(keystrokeDictionary['FindAccount_FPcode'][1])
+        sleep(0.2)
+        keyboard.release(keystrokeDictionary['FindAccount_FPcode'][0])
+        keyboard.release(keystrokeDictionary['FindAccount_FPcode'][1])
+        cb.copy(customerCode)
+        keyboard.press_and_release('menu')
+        for i in range(0,4):
+            keyboard.press_and_release('down')
+            sleep(0.2)
+        for i in range(0,5):
+            keyboard.press_and_release('enter')
+            sleep(0.3)
+
+def enableOneTimeMessage(customerCode):
+    print(Fore.YELLOW + '[PROCESSING]\t Enabling One-Time-Message for account with code:', customerCode + Fore.RESET)
+    keyboard.press(keystrokeDictionary['OneTimeMsg_Enable'][0])
+    keyboard.press(keystrokeDictionary['OneTimeMsg_Enable'][1])
+    sleep(0.2)
+    keyboard.press(keystrokeDictionary['OneTimeMsg_Enable'][0])
+    keyboard.press(keystrokeDictionary['OneTimeMsg_Enable'][1])
+    sleep(0.2)
+    keyboard.press(keystrokeDictionary['Tender_Cash'][0])
+    keyboard.press(keystrokeDictionary['Tender_Cash'][1])
+    print(Fore.GREEN + '[ENABLED]\t One-Time-Message for account with code:', customerCode + Fore.RESET)
+    openNewSale()
+         
+
+
